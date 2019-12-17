0844b1b8590a5a5ba86da5c2f54fea980895f716
Timoty Granziero <timoty.granziero@gmail.com>
2019-12-18 00:12:21 +0100

    Add actual random test

--- roller/dice.js
+++ roller/dice.js
@@ -3,11 +3,11 @@
 exports.diceRoller = {
 
     roll(sides, randomCallback = Math.random) {
-        return getRandomIntInclusive(1, sides, randomCallback)
+        return randInt(1, sides, randomCallback)
     }
 };
 
-function getRandomIntInclusive(min, max, randomCallback = Math.random) {
+function randInt(min, max, randomCallback = Math.random) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(randomCallback() * (max - min + 1)) + min;
--- test/dice.spec.js
+++ test/dice.spec.js
@@ -15,12 +15,17 @@ describe('Dice ranndom roller', () => {
         const rolled = diceRoller.roll(20, () => 0.9);
         expect(rolled).toBe(19);
     });
-    
+
     it('should return 20', () => {
         expect(diceRoller).toBeDefined();
 
         const rolled = diceRoller.roll(20, () => 0.9999);
         expect(rolled).toBe(20);
+    });
+
+    it('should be between 1 and 20 on an actual random call', () => {
+        expect(diceRoller.roll(20)).toBeGreaterThanOrEqual(0);
+        expect(diceRoller.roll(20)).toBeLessThanOrEqual(20);
     });
 
 });
